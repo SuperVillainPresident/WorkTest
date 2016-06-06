d71dda58bceaf21f4500006802d423904da861c0
Matthew <mrstalho@us.ibm.com>
2016-06-06 12:11:04 -0500

    commit

---
+++ src/dotnetstarter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+
+public class Startup
+{
+	public void Configure(IApplicationBuilder app)
+	{
+		app.UseDefaultFiles();
+		app.UseStaticFiles();
+        app.UseDirectoryBrowser();
+    }
+
+    public static void Main(string[] args) => WebApplication.Run(args);
+}
